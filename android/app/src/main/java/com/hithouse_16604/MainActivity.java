bd405c9026040aa2e405f44ea611b715934b2dbb
Crowdbotics <team@crowdbotics.com>
2020-05-04 20:58:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hithouse_16604/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hithouse_16604;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "hithouse_16604";
+    }
+}
